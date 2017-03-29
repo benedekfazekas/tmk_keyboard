bf0df2b3c94a320f655b170be374acc898cdcb9e
Benedek Fazekas <benedek.fazekas@gmail.com>
2017-03-29 17:52:15 +0100

    fix alt, super swap on secondary layers

--- keyboard/atreus/keymap_common.h
+++ keyboard/atreus/keymap_common.h
@@ -74,14 +74,14 @@ extern const uint16_t fn_actions[];
                             KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),   \
                           SHIFT(KC_5), SHIFT(KC_6), KC_LBRC, KC_RBRC, SHIFT(KC_GRAVE), \
                             SHIFT(KC_7), KC_1, KC_2, KC_3, KC_BSLS,       \
-                          KC_FN1, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, \
+                          KC_FN1, SHIFT(KC_INS), KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LGUI, \
                             KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL)
 
 /*
  *  !    @     up     {    }        ||     pgup    7     8     9    *
  *  #  left   down  right  $        ||     pgdn    4     5     6    +
  *  [    ]      (     )    &        ||       `     1     2     3    \
- * L2  insert super shift bksp ctrl || alt space   fn    .     0    =
+ * L2  insert alt shift bksp ctrl || super space   fn    .     0    =
  */
 #define FN_ARROW_LAYER   KEYMAP(SHIFT(KC_1), SHIFT(KC_2), KC_UP, KC_HOME, KC_END, \
                                   KC_PGUP, KC_7, KC_8, KC_9, SHIFT(KC_8), \
@@ -89,14 +89,14 @@ extern const uint16_t fn_actions[];
                                   KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL), \
                                 KC_DEL, KC_LBRC, SHIFT(KC_9),  SHIFT(KC_LBRC), SHIFT(KC_7), \
                                   KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS,    \
-                                KC_FN1, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, \
+                                KC_FN1, SHIFT(KC_INS), KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, \
                                   KC_LALT, KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL)
 
 /*
  * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
  *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
  * mouse volup             reset      ||             F1    F2    F3   F12
- *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
+ *       voldn  alt shift bksp ctrl || super space   L0  prtsc scroll pause
  */
 #define LAYER_TWO KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, \
                            KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
@@ -104,7 +104,7 @@ extern const uint16_t fn_actions[];
                            KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,          \
                          KC__VOLUP, KC_RBRC, SHIFT(KC_0), SHIFT(KC_RBRC), KC_FN3, \
                            KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,            \
-                         KC__VOLDOWN, KC_NO, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, \
+                         KC__VOLDOWN, KC_NO, KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, \
                            KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE)
 
 enum function_id {
