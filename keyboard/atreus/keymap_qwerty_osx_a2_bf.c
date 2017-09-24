6afea66c59125df3e0c32eff1890b1c1e91cfeb6
Benedek Fazekas <benedek.fazekas@gmail.com>
2017-09-24 14:51:32 +0100

    A2 tweaks
    (very likely i connected middle fn keys in the wrong order :/)

--- keyboard/atreus/keymap_qwerty_osx_a2_bf.c
+++ keyboard/atreus/keymap_qwerty_osx_a2_bf.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_TAB, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, SHIFT(KC_3), KC_LALT, KC_LSFT, KC_BSPC, KC_FN4, KC_LCTL, \
+         KC_ESC, SHIFT(KC_3), KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_FN4, \
          KC_FN1, KC_FN0, KC_MINS, KC_QUOT, KC_SCLN),                     \
   /* 1: punctuation and numbers */
   KEYMAP(SHIFT(KC_1), SHIFT(KC_2), KC_UP, KC_HOME, KC_END, \
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),                    \
          KC_DEL, KC_LBRC, SHIFT(KC_9),  SHIFT(KC_LBRC), SHIFT(KC_7),    \
          KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS,                           \
-         KC_ESC, SHIFT(KC_INS), KC_LALT, KC_LSFT, KC_BSPC, KC_FN4,     \
-         KC_LCTL, KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL),
+         KC_ESC, SHIFT(KC_INS), KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL,     \
+         KC_FN4, KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL),
   /* 2: arrows and function keys */
   KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP,              \
          KC_UP, KC_F7, KC_F8, KC_F9, KC_F10,                          \
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,                          \
          KC__VOLUP, KC_RBRC, SHIFT(KC_0), SHIFT(KC_RBRC), KC_FN3,       \
          KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,                            \
-         KC__VOLDOWN, KC_NO, KC_LALT, KC_LSFT, KC_BSPC, KC_FN4,        \
-         KC_LCTL, KC_FN1, KC_FN0, KC_PSCREEN, KC_SLCK, KC_PAUSE)
+         KC__VOLDOWN, KC_NO, KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL,        \
+         KC_FN4, KC_FN1, KC_FN0, KC_PSCREEN, KC_SLCK, KC_PAUSE)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
